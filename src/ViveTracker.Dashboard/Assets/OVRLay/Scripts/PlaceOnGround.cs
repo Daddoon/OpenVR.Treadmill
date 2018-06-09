2f663b6cae7c5e7ce025213d74f9020b4b918b9f
Daddoon <guillaume.zahra@daddoon.com>
2018-06-09 18:18:01 +0200

    Added OVRLay data to project in VivTracker.Dashboard

---
+++ src/ViveTracker.Dashboard/Assets/OVRLay/Scripts/PlaceOnGround.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceOnGround : MonoBehaviour {
+
+    public GameObject ParentObject;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (ParentObject == null)
+            return;
+
+        transform.position = new Vector3(ParentObject.transform.position.x, transform.position.y, ParentObject.transform.position.z);
+	}
+}
